de5071249748457d03c2d87800df8cc78c34aeb1
varadlakhotiya <lakhotiya.varad20@gmail.com>
2026-04-13 11:23:53 +0530

    Remove build artifacts

--- backend/target/classes/db/migration/V12__seed_customer_role.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO roles (role_code, role_name, description)
-SELECT 'CUSTOMER', 'Customer', 'Customer using CampusAdda ordering portal'
-WHERE NOT EXISTS (
-    SELECT 1 FROM roles WHERE role_code = 'CUSTOMER'
-);
