9f00ca6fed59f94ac7b42217627a907ec5ace173
varadlakhotiya <lakhotiya.varad20@gmail.com>
2026-05-28 19:21:10 +0530

    Disabled Kafka in production

--- backend/src/main/java/com/campusadda/vendorops/outbox/producer/KafkaEventProducer.java
+++ backend/src/main/java/com/campusadda/vendorops/outbox/producer/KafkaEventProducer.java
@@ -1,11 +1,17 @@
 package com.campusadda.vendorops.outbox.producer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(
+    name = "app.kafka.enabled",
+    havingValue = "true"
+)
 public class KafkaEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
