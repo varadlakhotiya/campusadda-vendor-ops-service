a684de9c2918267bc1c222c90826c6627bda82b1
varadlakhotiya <lakhotiya.varad20@gmail.com>
2026-05-28 15:20:14 +0530

    Dashboard updates

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "http://localhost:8081/api/v1",
+  API_BASE_URL: "https://campusadda-vendor-ops-service.onrender.com/api/v1",
   TOKEN_KEY: "vendor_ops_access_token",
   USER_KEY: "vendor_ops_current_user"
 };
